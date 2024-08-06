cde9e62861d964e5262b7c1cbfdfadd08e0672e7
leticianeresilva <161316884+leticianeresilva@users.noreply.github.com>
2024-08-06 16:07:07 -0300

    Add files via upload

---
+++ desafios/desafio a 12/java1.js
@@ -0,0 +1,17 @@
+var jogahtml = window.document.getElementById('saida')
+
+function clique1() {
+    jogahtml.innerHTML += `<p> Você escolheu a primeira opção = botão 1</p>`
+}
+
+function clique2() {
+    jogahtml.innerHTML += `<p> Você escolheu a segunda opção = botão 2</p>`
+}
+
+function clique3() {
+    jogahtml.innerHTML += `<p> Você escolheu a terceira opção = botão 3</p>`
+}
+
+function clique() {
+    jogahtml.innerHTML += `<p> Você escolheu a quarta opção = botão 4</p>`
+}
