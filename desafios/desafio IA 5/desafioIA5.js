f87f26f8e18af38993fa51f89cf0ea246977a313
leticianeresilva <161316884+leticianeresilva@users.noreply.github.com>
2024-11-14 08:11:23 -0300

    Add files via upload

---
+++ desafios/desafio IA 5/desafioIA5.js
@@ -0,0 +1,6 @@
+function toggleImage() {
+    const img1 = document.getElementById('image1');
+    const img2 = document.getElementById('image2');
+    img1.classList.toggle('active');
+    img2.classList.toggle('active');
+}
